fd9c4d5c2d792a017948ce65b7040f4d4caf9023
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-11-24 23:31:05 +0700

    eficient 16

--- python/016.py
+++ python/016.py
@@ -2,11 +2,6 @@
 #
 # What is the sum of the digits of the number 2^1000
 
-def dsum(n):
-    if n < 10: return n
-    else: return n%10 + dsum(n//10)
-
-print(dsum(2**1000))
+print(sum(int(n) for n in str(2**1000)))
 
 # output: 1366
-# TODO: print(sum(int(n) for n in str(2**1000)))
