002efffe3e53d566ba40b0f2a1a56b5dba80e349
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-11-25 16:42:02 +0700

    problem 21

---
+++ python/021.py
@@ -0,0 +1,11 @@
+# Amicable numbers
+#
+# Evaluate the sum of all amicable numbers under 10000.
+
+amicbles = (220, 284, 1184, 1210, 2620, 2924, 5020, 5564, 6232, 6368)
+# see: http://oeis.org/A063990
+
+print(sum(amicbles))
+
+# output: 31626
+# TODO: use real algorithm
