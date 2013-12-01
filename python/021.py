e3d561d427f9b208a5d80fb53d6c6247e2f406b2
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-12-01 23:21:25 +0700

    problem 21 dengan algoritma yang benar

--- python/021.py
+++ python/021.py
@@ -5,7 +5,15 @@
 amicables = (220, 284, 1184, 1210, 2620, 2924, 5020, 5564, 6232, 6368)
 # see: http://oeis.org/A063990
 
-print(sum(amicables))
+largest = 10001
+d = [0 for _ in range(largest)]
+
+for n in range(1,largest):
+    for k in range(2*n,largest,n):
+        d[k] += n
+
+ami = [n for n in range(largest) if
+         d[n] < largest and d[n] != n and d[d[n]] == n ]
+print(sum(ami))
 
 # output: 31626
-# TODO: use real algorithm
