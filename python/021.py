62fa8899772af5df4ad594740ce55e1eec5f0cc3
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-12-01 19:31:28 +0700

    amicbables, not amicbles

--- python/021.py
+++ python/021.py
@@ -2,10 +2,10 @@
 #
 # Evaluate the sum of all amicable numbers under 10000.
 
-amicbles = (220, 284, 1184, 1210, 2620, 2924, 5020, 5564, 6232, 6368)
+amicables = (220, 284, 1184, 1210, 2620, 2924, 5020, 5564, 6232, 6368)
 # see: http://oeis.org/A063990
 
-print(sum(amicbles))
+print(sum(amicables))
 
 # output: 31626
 # TODO: use real algorithm
