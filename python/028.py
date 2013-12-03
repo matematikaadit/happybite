c0776a0438dab6db11c6a60c836c785051cedf0c
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-12-03 18:17:02 +0700

    problem 28

---
+++ python/028.py
@@ -0,0 +1,12 @@
+# Number spiral diagonals
+#
+# What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral
+# formed in the same way?
+def spiral(n):
+    s = 1
+    for n in range(3,n+1,2):
+        s += 4*n*n - 6*(n-1) # by finding the pattern
+    return s
+
+print(spiral(1001))
+# output: 669171001
