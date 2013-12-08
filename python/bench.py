773c7372b82e0ff522fdf4cae97d83a25efc36d7
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-12-08 17:54:14 +0700

    bencmark refactoring

--- python/bench.py
+++ python/bench.py
@@ -2,13 +2,19 @@ from glob import glob
 import time
 import subprocess as subp
 
-def bench(f):
+def bench(f,*arg):
     start = time.perf_counter()
-    f()
+    f(*arg)
     end = time.perf_counter()
     return end - start
 
-for script in sorted(glob('???.py')):
-    d = bench(lambda :subp.call(['python3',script],stdout=subp.DEVNULL))
-    if d > 1:
-        print("{} ({:.1f} s)".format(script,d))
+fs = sorted(glob('???.py'))
+cmd = lambda s:subp.call(['python3',s],stdout=subp.DEVNULL)
+def run(fs=fs):
+    for script in fs:
+        d = bench(cmd,script)
+        if d > 1:
+            print("{} ({:.1f} s)".format(script,d))
+
+if __name__ == '__main__':
+    run()
