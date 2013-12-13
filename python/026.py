3dff7f2aae6073b70da141daed7e2b74ad1e59ce
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-12-13 18:46:01 +0700

    solved problem 26

---
+++ python/026.py
@@ -0,0 +1,29 @@
+# Reciprocal Cycle
+#
+# Find the value of d < 1000 for which 1/d contains the longest recurring
+# cycle in its decimal fraction part.
+
+def recycle(n):
+    pos = {}
+    a = 1
+    for i in range(0,n):
+        if a in pos:
+            return i - pos[a]
+        if a == 0:
+            return 0
+        pos[a] = i
+        a = (a*10) % n
+    return 0
+
+rest = 0
+num = 0
+
+for d in range(999,0,-1):
+    if d <= rest: break
+
+    c = recycle(d)
+    if c > rest:
+        rest, num = c, d
+
+print(num)
+# output: 983
