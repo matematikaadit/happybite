24812567f1891bbc9b02de1338daa1d463707c14
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-12-15 23:50:08 +0700

    trunctaed prime attempt

---
+++ python/trunct.py
@@ -0,0 +1,105 @@
+# IPython log file
+
+import primes
+primes.right(513)
+primes.right(313)
+primes.right(713)
+get_ipython().system('factor 713')
+get_ipython().system('factor 313')
+primes.right(253)
+primes.right(353)
+primes.right(373)
+primes.gen(100)
+len(primes.gen(1000))
+[ x for x in primes.gen(1000) if x % 100 == 13]
+[ x for x in primes.gen(1000) if x % 100 == 73]
+[ x for x in primes.gen(1000) if x % 100 == 11]
+[ x for x in primes.gen(1000) if x % 100 == 31]
+[ x for x in primes.gen(1000) if x % 100 == 17]
+[ x for x in primes.gen(1000) if x % 100 == 37]
+[ x for x in primes.gen(1000) if x % 100 == 77]
+primes.right(3113)
+primes.right(911)
+primes.right(9113)
+primes.right(3173)
+primes.right(773)
+primes.right(9773)
+[ x for x in primes.gen(1000) if x % 100 == 31]
+[ x for x in primes.gen(10000) if x % 100 == 31]
+[ x for x in primes.gen(10000) if x % 1000 == 113]
+[ x for x in primes.gen(1000) if x % 100 == 13]
+[ x for x in primes.gen(1000) if x % 100 == 73]
+[ x for x in primes.gen(10000) if x % 1000 == 113]
+[ x for x in primes.gen(10000) if x % 1000 == 313]
+[ x for x in primes.gen(10000) if x % 1000 == 173]
+[ x for x in primes.gen(10000) if x % 1000 == 373]
+[ x for x in primes.gen(10000) if x % 1000 == 773]
+[ x for x in primes.gen(100000) if x % 10000 == 3313]
+[ x for x in primes.gen(100000) if x % 10000 == 9173]
+[ x for x in primes.gen(100000) if x % 10000 == 1373]
+[ x for x in primes.gen(100000) if x % 10000 == 3373]
+[ x for x in primes.gen(1000000) if x % 100000 == 13313]
+[ x for x in primes.gen(1000000) if x % 100000 == 99173]
+[ x for x in primes.gen(1000000) if x % 100000 == 91373]
+[ x for x in primes.gen(10000000) if x % 100000 == 399173]
+[ x for x in primes.gen(10000000) if x % 1000000 == 399173]
+[ x for x in primes.gen(10000000) if x % 1000000 == 391373]
+[ x for x in primes.gen(100) if x % 10 == 7]
+import math
+math.floor(math.log10(373))
+math.floor(math.log10(3734))
+math.floor(math.log10(3))
+[ x for x in primes.gen(1000) if x % 100 == 17]
+[ x for x in primes.gen(1000) if x % 100 == 37]
+[ x for x in primes.gen(1000) if x % 100 == 97]
+[ x for x in primes.gen(10000) if x % 1000 == 797]
+[ x for x in primes.gen(10000) if x % 1000 == 79]
+[ x for x in primes.gen(10000) if x % 1000 == 317]
+[ x for x in primes.gen(10000) if x % 1000 == 137]
+[ x for x in primes.gen(10000) if x % 1000 == 337]
+[ x for x in primes.gen(10000) if x % 1000 == 937]
+[ x for x in primes.gen(10000) if x % 1000 == 793]
+get_ipython().system('factor 793')
+get_ipython().system('factor 79')
+[ x for x in primes.gen(10000) if x % 1000 == 197]
+[ x for x in primes.gen(10000) if x % 1000 == 397]
+[ x for x in primes.gen(10000) if x % 1000 == 797]
+[ x for x in primes.gen(10000) if x % 1000 == 997]
+[ x for x in primes.gen(100000) if x % 10000 == 1997]
+[ x for x in primes.gen(100000) if x % 10000 == 3797]
+[ x for x in primes.gen(100000) if x % 10000 == 7937]
+[ x for x in primes.gen(100000) if x % 10000 == 9137]
+[ x for x in primes.gen(100000) if x % 10000 == 9337]
+[ x for x in primes.gen(100000) if x % 10000 == 793]
+[ x for x in primes.gen(100000) if x % 10000 == 9337]
+[ x for x in primes.gen(100000) if x % 10000 == 3137]
+[ x for x in primes.gen(100000) if x % 10000 == 9137]
+get_ipython().system('factor 379')
+get_ipython().system('factor 37')
+[ x for x in primes.gen(100000) if x % 10000 == 9367]
+[ x for x in primes.gen(100000) if x % 10000 == 9337]
+[ x for x in primes.gen(100000) if x % 10000 == 9397]
+get_ipython().system('factor 79')
+get_ipython().system('factor 793')
+[ x for x in primes.gen(100000) if x % 10000 == 3797]
+[ x for x in primes.gen(100000) if x % 10000 == 1997]
+[ x for x in primes.gen(1000000) if x % 100000 == 1997]
+get_ipython().system('factor 91997')
+[ x for x in primes.gen(1000000) if x % 100000 == 91997]
+[ x for x in primes.gen(1000000) if x % 100000 == 33797]
+[ x for x in primes.gen(1000000) if x % 100000 == 99397]
+[ x for x in primes.gen(1000000) if x % 100000 == 79397]
+get_ipython().system('factor 379')
+get_ipython().system('factor 3793')
+get_ipython().system('factor 37939')
+get_ipython().system('factor 79')
+get_ipython().system('factor 793')
+[ x for x in primes.gen(1000000) if x % 100000 == 39397]
+get_ipython().system('factor 739')
+get_ipython().system('factor 7393')
+get_ipython().system('factor 73939')
+get_ipython().system('factor 739397')
+[739397,3137,3797,317,797,37,313,373,23,53,73]
+sum(_)
+get_ipython().magic('logstart trunct.py')
+exit()
