f7354a3c8e1e9113832929b0302a37ac1c17fd4a
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2013-12-29 23:31:15 +0700

    tambah prime test di lib

--- go/lib/primes/primes.go
+++ go/lib/primes/primes.go
@@ -31,3 +31,18 @@ func State(n int) []bool {
 
 	return state
 }
+
+func Test(n int) bool {
+	if n < 2 {
+		return false
+	}
+	if n == 2 {
+		return true
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
