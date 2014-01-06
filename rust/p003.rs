2100fb25e2d1ed089d751ab36a2c7c04b41da44d
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-01-06 17:11:32 +0700

    fix tab in problem 3 rust

--- rust/p003.rs
+++ rust/p003.rs
@@ -1,17 +1,17 @@
 fn main() {
-	let mut	n: i64 = 600851475143;
-	let mut	k: i64 = 2;
-	let mut	last: i64 = k;
-	while n > 1 {
-		if n%k == 0 {
-			last = k;
-			while n%k == 0 {
-				n /= k;
-			}
-		}
-		k += 1;
-	}
-	println(last.to_str())
-	// output: 6857
+    let mut    n: i64 = 600851475143;
+    let mut    k: i64 = 2;
+    let mut    last: i64 = k;
+    while n > 1 {
+        if n%k == 0 {
+            last = k;
+            while n%k == 0 {
+                n /= k;
+            }
+        }
+        k += 1;
+    }
+    println(last.to_str())
+    // output: 6857
 }
 
