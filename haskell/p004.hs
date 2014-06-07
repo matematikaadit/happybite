cc6ffc5a1647d800a465b8f6b0258cd51798f054
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-06-08 05:18:42 +0700

    refactoring problem 4

--- haskell/p004.hs
+++ haskell/p004.hs
@@ -5,14 +5,15 @@ reverseNum :: Int -> Int
 reverseNum = read . reverse . show
 
 largestPalProd :: Int -> Int -> Int
-largestPalProd up down = f up up 0
+largestPalProd top down = f top top minBound
     where
       f                     :: Int -> Int -> Int -> Int
       f a b n
-        | a < down          = n
-        | b < a || a*b <= n = f (a-1) up n
-        | palindrome (a*b)  = f a (b-1) (a*b)
-        | otherwise         = f a (b-1) n
+        | a < down        = n
+        | b < a || c <= n = f (a-1) top n
+        | palindrome c    = f a (b-1) c
+        | otherwise       = f a (b-1) n
+        where c = a * b
 
 main :: IO ()
 main = print $ largestPalProd 999 100
